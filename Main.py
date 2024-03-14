22168ef6051e6976c8f459b4c8ed4ee1ee730fd6
Vignesh Saravanakumar <10021913@sbstudents.org>
2024-03-14 11:34:27 -0400

    Players
    Signed-off-by: Vignesh Saravanakumar <10021913@sbstudents.org>

--- Main.py
+++ Main.py
@@ -15,28 +15,22 @@ print("\033[91mIPL Fantasy Points Calculator:\033[0m\n")
 #! Batting Calculations
 print("\033[92mBatting Calculations...\033[0m")
 print("\033[90mRuns, Strike Rate, 0s, 50s, 100s, NOs\033[0m")
-
 response = get(BATTING_DATA_SOURCE)
 batting_data = format_batter_table(response.text)
-
 print("\n")
 
 
 #! Bowling Calculations
 print("\033[92mBowling Calculations...\033[0m")
 print("\033[90mWickets, Maidens, Hat-Tricks, 4+ Wickets, 5+ Wickets, 6+ Wickets, Economy Rate\033[0m")
-
 response = get(BOWLING_DATA_SOURCE)
 bowling_data = format_bowler_table(response.text)
-
 print("\n")
 
 
 #! Create Players and Teams
 print("\033[92mCreate Players and Teams\033[0m")
-
-
-# Now, create teams using the method
 teams = []
 teams.append(create_team(batting_data, bowling_data, "Team O", O))
 teams.append(create_team(batting_data, bowling_data, "Team W", W))
+
